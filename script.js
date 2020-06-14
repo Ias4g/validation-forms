123881e9bf6ddfca67c8f628f18ba5664ad7307c
Izael silva <izaell.universitario@outlook.com>
2020-06-14 16:48:34 -0300

    Code refactoring

--- script.js
+++ script.js
@@ -6,22 +6,16 @@ document.querySelector("form")
     })
 
 const fields = document.querySelectorAll("[required]")
-// console.log(fields)
 
-function customValidation(event) {
-    // eliminar o bubble
-    event.preventDefault()
-
-    const field = event.target
-
-    // logica para erificar se exixtem erros
+function validateField(field) {
+    // logica para verificar se existem erros
     function verifyErros() {
         let foundError = false
 
         for (let error in field.validity) {
             // se não for customError
             // então verifica se tem erro 
-            if (error != "cutomError" && field.validity[error]) {
+            if (field.validity[error] && !field.validity.valid) {
                 foundError = error
             }
         }
@@ -29,17 +23,59 @@ function customValidation(event) {
         return foundError
     }
 
-    const error = verifyErros()
-    console.log("Error exist: ", error)
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo"
+            },
+            email: {
+                valueMissing: "O email é obrigatório",
+                typeMismatch: "Por favor insira um formato de email válido"
+            }
+        }
 
-    // if (error) {
-    //     field.setCustomValidity("Este campo é obrigatório")
-    // } else {
-    //     field.setCustomValidity("")
-    // }
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.querySelector("span.error")
+
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+        } else {
+            spanError.classList.remove("active")
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function () {
+        const error = verifyErros()
+
+        if (error) {
+            const message = customMessage(error)
+            field.style.borderColor = "rgba(255, 0, 0, 0.3)"
+            setCustomMessage(message)
+        } else {
+            field.style.borderColor = "rgba(0, 255, 0, 0.3)"
+            setCustomMessage()
+        }
+    }
 }
 
-// for(let field of fields)
+function customValidation(event) {
+    const field = event.target
+    const validation = validateField(field)
+
+    validation()
+}
+
+// for(let field of fields) outra opção
 for (field of fields) {
-    field.addEventListener("invalid", customValidation)
+    field.addEventListener("invalid", event => {
+        // eliminar o bubble
+        event.preventDefault()
+        customValidation(event)
+    })
+    field.addEventListener("blur", customValidation)
 }
